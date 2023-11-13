2c950b8c7c83e6cc351d06826fab08ca5cb9dea7
J Gdovic <jgdovic3@gmail.com>
2023-11-13 13:55:08 -0500

    made table to display mountain information

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -2,7 +2,7 @@
 // get elements into variables
 
 const mountainSelect = document.querySelector("#mountainSelect");
-
+const mountainsTbody = document.querySelector("#mountainsTbody");
 
 //functions
 
@@ -17,11 +17,32 @@ function loadMountainDropDown() {
     
 }
 
-function showMountainTable() {
-    
+function showMountainTable(mountainName) {
+    for (const mountain of mountainsArray) {
+        if(mountain.name == mountainName){
+            let tableRow = mountainsTbody.insertRow(-1);
+
+            let td1 = tableRow.insertCell(0);
+            td1.innerText = mountain.name;
+            
+            let td2 = tableRow.insertCell(1);
+            td2.innerText = mountain.desc;
+
+            let td3 = tableRow.insertCell(2);
+            td3.innerText = mountain.elevation;
+
+            let td5 = tableRow.insertCell(3);
+            td5.innerText = mountain.effort;
+        }
+    }
 }
 
-
+function changeMountain() {
+    mountainsTbody.innerHTML = "";
+    const mountainName = mountainSelect.value;
+    showMountainTable(mountainName);
+}
 
 //wire up
 loadMountainDropDown();
+mountainSelect.onchange = changeMountain;
