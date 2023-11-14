b50397d93b7402cee2e8a14a68b46575371e73ac
J Gdovic <jgdovic3@gmail.com>
2023-11-14 16:43:00 -0500

    some css

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -7,46 +7,42 @@ const imageDiv = document.querySelector("#imageDiv");
 
 //functions
 
-
 function loadMountainDropDown() {
-    for (const mountain of mountainsArray) {
-        let option = document.createElement("option");
-        option.value = mountain.name;
-        option.innerText = mountain.name;
-        mountainSelect.appendChild(option);
-    }
-    
+  for (const mountain of mountainsArray) {
+    let option = document.createElement("option");
+    option.value = mountain.name;
+    option.innerText = mountain.name;
+    mountainSelect.appendChild(option);
+  }
 }
 
 function showMountainTable(mountainName) {
+  for (const mountain of mountainsArray) {
+    if (mountain.name == mountainName) {
+      let tableRow = mountainsTbody.insertRow(-1);
 
-    for (const mountain of mountainsArray) {
-        
-        if(mountain.name == mountainName){
-            let tableRow = mountainsTbody.insertRow(-1);
+      let td1 = tableRow.insertCell(0);
+      td1.innerText = mountain.name;
 
-            let td1 = tableRow.insertCell(0);
-            td1.innerText = mountain.name;
-            
-            let td2 = tableRow.insertCell(1);
-            td2.innerText = mountain.desc;
+      let td2 = tableRow.insertCell(1);
+      td2.innerText = mountain.desc;
 
-            let td3 = tableRow.insertCell(2);
-            td3.innerText = mountain.elevation;
+      let td3 = tableRow.insertCell(2);
+      td3.innerText = mountain.elevation;
 
-            let td5 = tableRow.insertCell(3);
-            td5.innerText = mountain.effort;
+      let td5 = tableRow.insertCell(3);
+      td5.innerText = mountain.effort;
 
-            let td6 = tableRow.insertCell(4);
-            td6.innerHTML = `<img src="images/${mountain.img}`;
-        }
+      let td6 = tableRow.insertCell(4);
+      td6.innerHTML = mountain.img;
     }
+  }
 }
 
 function changeMountain() {
-    mountainsTbody.innerHTML = "";
-    const mountainName = mountainSelect.value;
-    showMountainTable(mountainName);
+  mountainsTbody.innerHTML = "";
+  const mountainName = mountainSelect.value;
+  showMountainTable(mountainName);
 }
 
 //wire up
